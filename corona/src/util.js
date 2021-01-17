a6be3a8177d9f67dbc80e2e7b489d556ba3eb8e5
vistritpandey <vistritpandey@gmail.com>
2021-01-17 15:45:51 +0530

    k

--- corona/src/util.js
+++ corona/src/util.js
@@ -4,22 +4,16 @@ import { Circle, Popup } from "react-leaflet";
 
 const casesTypeColors = {
   cases: {
-    hex: "#CC1034",
-    rgb: "rgb(204, 16, 52)",
-    half_op: "rgba(204, 16, 52, 0.5)",
-    multiplier: 250,
+    multiplier: 300,
+    option: { color: "#0000ff", fillColor: "#87ceeb" },
   },
   recovered: {
-    hex: "#7dd71d",
-    rgb: "rgb(125, 215, 29)",
-    half_op: "rgba(125, 215, 29, 0.5)",
-    multiplier: 200,
+    multiplier: 250,
+    option: { color: "#7dd71d", fillColor: "#7dd71d" },
   },
   deaths: {
-    hex: "#fb4443",
-    rgb: "rgb(251, 68, 67)",
-    half_op: "rgba(251, 68, 67, 0.5)",
-    multiplier: 1000,
+    multiplier: 200,
+    option: { color: "#cc1034", fillColor: "#cc1034" },
   },
 };
 
@@ -38,13 +32,12 @@ export const sortData = (data) => {
 export const prettyPrintStat = (stat) =>
   stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
-export const showDataOnMap = (data, casesType = "cases") =>
+export const showDataOnMap = (data, casesType) =>
   data.map((country) => (
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
-      color={casesTypeColors[casesType].hex}
-      fillColor={casesTypeColors[casesType].hex}
-      fillOpacity={0.4}
+      fillOpacity={0.5}
+      pathOptions={casesTypeColors[casesType].option}
       radius={
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
